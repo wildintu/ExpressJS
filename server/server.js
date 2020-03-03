f51ed65c58b453729abd7c8204117cca03385799
wildintu <hello@wildintu.com>
2020-03-03 11:04:48 -0600

    middleware logs requested url

--- server/server.js
+++ server/server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const path =  require('path');
 let app = express();
 
+//super special middleware logger
+app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+})
+
+// app.get('/', (req, res) => {
+//     res.send('Hello from the web server side...')
+// })
+
 app.use(express.static(path.join(__dirname, '../public')));
+
+// app.get('/order/:id', (req, res) => {
+//     let id = req.params.id;
+//     res.send(id);
+// });
 
 app.listen(3000);
