7f285f724ff94a66b5d61852f83d24cc3e921d42
wildintu <hello@wildintu.com>
2020-03-03 16:42:02 -0600

    finished with advanced

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path =  require('path');
+const bodyParser =  require('body-parser');
+const fs = require('fs');
 let app = express();
+
+let dataPath = path.join(__dirname, '../data.json');
 
 //super special middleware logger
 app.use((req, res, next) => {
@@ -18,5 +22,21 @@ app.use(express.static(path.join(__dirname, '../public')));
 //     let id = req.params.id;
 //     res.send(id);
 // });
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.post('/forms-submissions', (req, res) => {
+    let po = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+
+fs.writeFile(dataPath, JSON.stringify(po), (err) => {
+    if (err) console.log(err);
+});
+    res.send(po);
+})
 
 app.listen(3000);
