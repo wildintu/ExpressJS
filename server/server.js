5d00e1e92f61226fb68f350d0a03c9e1b781fc3b
wildintu <hello@wildintu.com>
2020-03-03 10:37:16 -0600

    added middleware component

--- server/server.js
+++ server/server.js
@@ -2,11 +2,6 @@ const express = require('express');
 const path =  require('path');
 let app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/css/styles.css', (req, res) => {
-    res.sendFile(path.join(__dirname,'../public/css/styles.css'));
-})
 app.listen(3000);
