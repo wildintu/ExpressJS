95b2c2a2d3e84e44bc8ad4919f2380959bea258d
wildintu <hello@wildintu.com>
2020-03-03 10:33:34 -0600

    applying css styles

--- server/server.js
+++ server/server.js
@@ -6,4 +6,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+app.get('/css/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/css/styles.css'));
+})
 app.listen(3000);
